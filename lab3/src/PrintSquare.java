e2844425a1910c2098e79ff88dc00186db53f58a
Anton Eliasson <devel@antoneliasson.se>
2011-09-28 11:01:14 +0200

    Gjort klart uppgift 2 på labb 3.

---
+++ lab3/src/PrintSquare.java
@@ -0,0 +1,18 @@
+import se.lth.cs.ptdc.window.SimpleWindow;
+import se.lth.cs.ptdc.square.Square;
+
+public class PrintSquare {
+	public static void main(String[] args) {
+		SimpleWindow w = new SimpleWindow(600, 600, "PrintSquares");
+		Square sq = new Square(300, 300, 100);
+		sq.draw(w);
+		
+		while(true) {
+			w.waitForMouseClick();
+			sq.erase(w);
+			sq.move(w.getMouseX() - sq.getX(), w.getMouseY() - sq.getY());
+			//System.out.println("X=" + sq.getX() + ", Y=" + sq.getY());
+			sq.draw(w);
+		}
+	}
+}
