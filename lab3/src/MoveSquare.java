28af5bf142180062ece4b795432439f9757238b8
Anton Eliasson <devel@antoneliasson.se>
2011-09-28 11:04:23 +0200

    Bytte namn på klassen i labb 3, uppgift 2

--- lab3/src/MoveSquare.java
+++ lab3/src/MoveSquare.java
@@ -1,7 +1,7 @@
 import se.lth.cs.ptdc.window.SimpleWindow;
 import se.lth.cs.ptdc.square.Square;
 
-public class PrintSquare {
+public class MoveSquare {
 	public static void main(String[] args) {
 		SimpleWindow w = new SimpleWindow(600, 600, "PrintSquares");
 		Square sq = new Square(300, 300, 100);
