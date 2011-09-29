e751249ec98082e68fbbc17d4d80bb0e30c8d92f
Anton Eliasson <devel@antoneliasson.se>
2011-09-29 16:42:17 +0200

    Gjort klart labb 3, uppgift 4

---
+++ lab3/src/MoveSquareWithColor.java
@@ -0,0 +1,26 @@
+import se.lth.cs.ptdc.window.SimpleWindow;
+import se.lth.cs.ptdc.square.Square;
+
+public class MoveSquareWithColor {
+	public static void main(String[] args) {
+		SimpleWindow w = new SimpleWindow(600, 600, "MoveSquare");
+		Square sq = new Square(300, 300, 100);
+		sq.draw(w);
+		
+		while(true) {
+			w.waitForEvent();
+			if (w.getEventType() == SimpleWindow.MOUSE_EVENT) {
+				sq.erase(w);
+				sq.move(w.getMouseX() - sq.getX(), w.getMouseY() - sq.getY());
+				//System.out.println("X=" + sq.getX() + ", Y=" + sq.getY());
+				sq.draw(w);
+			} else {
+				if (w.getKey() == 'r') {
+					w.setLineColor(new java.awt.Color(255, 0, 0));
+				} else {
+					w.setLineColor(new java.awt.Color(0, 0, 0));
+				}
+			}
+		}
+	}
+}
