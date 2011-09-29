8087189f4cc7a845a15753a0d25a0b174ef31180
Anton Eliasson <devel@antoneliasson.se>
2011-09-29 17:35:41 +0200

    Gjort klart till och med uppgift 2 på labb 4.

--- lab4/src/Turtle.java
+++ lab4/src/Turtle.java
@@ -1,7 +1,11 @@
 import se.lth.cs.ptdc.window.SimpleWindow;
 
 public class Turtle {
-	// ... lägg till attribut
+	private SimpleWindow w; // fönstret sköldpaddan befinner sig i
+	private double x; // nuvarande x-koordinat
+	private double y; // nuvarande y-koordinat
+	private int orientation = 270; // nuvarande orientering i grader, räknat motsols med noll rakt österut
+	private boolean penEnabled = false;
 	
 	/**
 	 * Skapar en sköldpadda som ritar i ritfönstret w. Från början befinner sig
@@ -9,27 +13,38 @@ public class Turtle {
 	 * i fönstret (i negativ y-riktning)
 	 */
 	public Turtle(SimpleWindow w, int x, int y) {
-		// ... implementera
+		this.w = w;
+		this.x = x;
+		this.y = y;
+		w.moveTo(x, y);
 	}
 	
 	/** Sänker pennan */
 	public void penDown() {
-		// ... implementera
+		penEnabled = true;
 	}
 	
 	/** Lyfter pennan */
 	public void penUp() {
-		// ... implementera
+		penEnabled = false;
 	}
 	
 	/** Går rakt framåt n pixlar i den riktning som huvudet pekar */
 	public void forward(int n) {
-		// ... implementera
+		x += n * Math.cos(Math.PI * orientation / 180);
+		y += n * Math.sin(Math.PI * orientation / 180);
+		
+		if (penEnabled) {
+			w.lineTo((int) Math.round(x), (int) Math.round(y));
+		} else {
+			w.moveTo((int) Math.round(x), (int) Math.round(y));
+		}
 	}
 	
 	/** Vrider beta grader åt vänster runt pennan */
 	public void left(int beta) {
-		// ... implementera
+		orientation -= beta;
+		orientation %= 360;
 	}
 	
 	/** Går till punkten newX,newY utan att rita. Pennans läge (sänkt 
